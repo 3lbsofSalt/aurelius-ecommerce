16e57969f85e60ac6a32d1ac5e2a5ce1588880a9
3lbsofSalt <max.ramsdell@protonmail.com>
2024-01-08 09:05:51 -0700

    Added a new compose file and started working on cart display in orders

--- pages/admin/orders/[id].vue
+++ pages/admin/orders/[id].vue
@@ -10,6 +10,14 @@ if(permissionsError.value || !routePrivs.value.includes('Inventory')) {
   navigateTo('/');
 }
 
+const cartHeaders = [
+  { title: 'Image', value: 'image' }, 
+  { title: 'Name', value: 'name' },
+  { title: 'Qty', value: 'quantity' },
+  { title: 'Price', value: 'price' },
+  { title: 'Total', value: 'total' }
+];
+
 const errorStore = useErrorStore();
 
 const route = useRoute();
@@ -49,7 +57,28 @@ if(orderError.value || !order.value) {
           title="Order Cart"
           class="sans-serif w-100"
         >
-          <v-row></v-row>
+          <v-data-table
+            show-expand
+            :headers="cartHeaders"
+          >
+          </v-data-table>
+          <v-container>
+
+            <v-row
+              v-for="item in order?.cart.items"
+            >
+              <v-col>
+              <v-img
+                width="80"
+                cover
+                aspect-ratio="1/1"
+                :src="baseImageUrl(item.item.baseImagePath || '', item.item.images[0])"
+              ></v-img>
+              </v-col>
+              <v-col>{{item.item.name}}</v-col>
+              <v-col>Qty: {{item.quantity}} @ {{item.item.price.toFixed(2)}} = {{(item.quantity * item.item.price)}}</v-col>
+            </v-row>
+          </v-container>
         </v-card>
       </v-col>
       <v-col class="pa-1">
@@ -58,6 +87,8 @@ if(orderError.value || !order.value) {
           title="Customer"
           class="sans-serif w-100"
         >
+          <v-container>
+          </v-container>
         </v-card>
       </v-col>
     </v-row>

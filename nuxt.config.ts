3df32a1dc4690dba041244f1074b0613ceb0f3a6
3lbsofSalt <max.ramsdell@protonmail.com>
2024-03-26 13:14:37 -0600

    Trying everything I can

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import defaultTheme from './assets/theme';
 export default defineNuxtConfig({
-
-  /*
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          /*
-          manualChunks(id) {
-            const chunks = {
-              '@sidebase/nuxt-auth': [
-                '@sidebase/nuxt-auth',
-                'node_modules/@sidebase/nuxt-auth/dist/runtime/server/services/authjs/nuxtAuthHandler.mjs',
-                'node_modules/@sidebase/nuxt-auth/dist/runtime/server/services/index.mjs'
-              ]
-            };            
-
-            for(const [chunkName, modules] of Object.entries(chunks)) {
-              if(modules.some(module => id.includes(module))) {
-                return chunkName;
-              }
-            }
-          }
-        }
-      }
-    }
-  },
-  */
-
   devtools: { enabled: true },
 
   css: [
